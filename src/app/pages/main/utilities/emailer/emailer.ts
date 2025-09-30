e3c71f62fa3e0ee2f501530ba22a172bd480a855
prakash <prakash.chaurasiya214522@gmail.com>
2025-09-30 17:42:01 +0530

    importfile and email

--- src/app/pages/main/utilities/emailer/emailer.ts
+++ src/app/pages/main/utilities/emailer/emailer.ts
@@ -1,11 +1,73 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { QuillEditorComponent } from 'ngx-quill';
 
 @Component({
   selector: 'app-emailer',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, MatButtonModule, QuillEditorComponent],
   templateUrl: './emailer.html',
-  styleUrl: './emailer.css'
+  styleUrls: ['./emailer.css']
 })
 export class Emailer {
 
+  emailForm: FormGroup;
+  products = ['Product A', 'Product B', 'Product C'];
+
+  quillConfig = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],
+      [{ 'indent': '-1'}, { 'indent': '+1' }],
+      [{ 'direction': 'rtl' }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['clean'],
+      ['link', 'image', 'video']
+    ]
+  };
+
+  constructor(private fb: FormBuilder) {
+    this.emailForm = this.fb.group({
+      product: ['All'],
+      recipientType: ['selected'],
+      status: ['Activated'],
+      to: [''],
+      messageType: ['Email'],
+      subject: [''],
+      message: ['']
+    });
+  }
+
+  onSelectGroup() {
+    console.log('Open recipient group modal');
+  }
+
+  onFillList() {
+    console.log('Fill recipient list');
+  }
+
+  onShowList() {
+    console.log('Show recipient list');
+  }
+
+  onMessageReport() {
+    console.log('Open message report');
+  }
+
+  onSendEmail() {
+    if (this.emailForm.valid) {
+      console.log('Form Data:', this.emailForm.value);
+      alert('Message Sent!');
+    } else {
+      alert('Please fill all required fields');
+    }
+  }
 }
--- src/app/pages/main/utilities/import-party/import-party.ts
+++ src/app/pages/main/utilities/import-party/import-party.ts
@@ -1,11 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { NgxSelectModule } from "ngx-select-ex";
 
 @Component({
   selector: 'app-import-party',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatButton, NgxSelectModule],
   templateUrl: './import-party.html',
   styleUrl: './import-party.css'
 })
 export class ImportParty {
+
+importForm: FormGroup;
+  categories = ['Supplier', 'Customer', 'Distributor'];
+  products = ['Product A', 'Product B', 'Product C'];
+  worksheets = ['Sheet1', 'Sheet2', 'Sheet3'];
+
+  selectedFile: File | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.importForm = this.fb.group({
+      category: [''],
+      product: [''],
+      worksheet: ['Sheet1'],
+      mode: ['new']
+    });
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      console.log('Selected File:', this.selectedFile.name);
+    }
+  }
+
+  onUpload() {
+    if (this.selectedFile) {
+      console.log('Uploading file:', this.selectedFile.name);
+      // TODO: implement API call for upload
+    } else {
+      alert('Please select a file first.');
+    }
+  }
+
+  onImport() {
+    console.log('Form Data:', this.importForm.value);
+    // TODO: implement API call for import
+  }
 
 }

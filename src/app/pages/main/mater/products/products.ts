b88dc12feee7510f8b596e6176e9a238b067daec
prakash <prakash.chaurasiya214522@gmail.com>
2025-10-06 17:19:10 +0530

    table is added

--- src/app/pages/main/mater/products/products.ts
+++ src/app/pages/main/mater/products/products.ts
@@ -1,20 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ColumnMode, NgxDatatableModule } from '@swimlane/ngx-datatable';
+
+
+interface Product {
+  ProductCode: number;
+  ProductName: string;
+  Description: string;
+  ActiveSince: string;
+  Rowstatus: string;
+}
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgxDatatableModule, FormsModule, CommonModule],
   templateUrl: './products.html',
-  styleUrl: './products.css'
+  styleUrls: ['./products.css']
 })
 export class Products implements OnInit {
 
-  http = inject(HttpClient);
+  private http = inject(HttpClient);
 
-  apiResponse: any;
+  apiResponse: Product[] = [];
 
-  ngOnInit(): any {
+  //Products: Product[] = [];
+
+  ColumnMode = ColumnMode;
+
+  columnDefs = [
+    { header: 'Product Code', prop: 'ProductCode', width: 75, sortable: true },
+    { header: 'Product Name', prop: 'ProductName', width: 75, sortable: true },
+    { header: 'Description', prop: 'Description', width: 140, sortable: true },
+    { header: 'Active Since', prop: 'ActiveSince', width: 85, sortable: true },
+    { header: 'Row Status', prop: 'Rowstatus', width: 100, sortable: true }
+  ];
+
+  ngOnInit(): void {
     this.loadLicProduct();
   }
 
@@ -43,7 +67,14 @@ export class Products implements OnInit {
           alert("Something went wrong!");
         }
       });
+  }
 
+  onPageChange(event: any) {
+    console.log(event);
+  }
+
+  onSortChange(event: any) {
+    console.log(event);
   }
 
 }
